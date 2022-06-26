b26ab6021e28994f37f71a4564ffeaa2a2da782b
mslxi <56199297+mslxi@users.noreply.github.com>
2022-06-27 00:16:52 +0800

    娱乐脚本之---hacker

---
+++ hacker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
+}
+function hacker ()
+{
+	green $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM
+        hacker
+}
+hacker
